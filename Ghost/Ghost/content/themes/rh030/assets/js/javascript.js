97c12189d74f849a2779b1872bb49ee72b9875e7
Simon Stellwag <stellwagsimon@gmail.com>
2013-12-20 12:47:58 +0100

    animation

--- Ghost/Ghost/content/themes/rh030/assets/js/javascript.js
+++ Ghost/Ghost/content/themes/rh030/assets/js/javascript.js
@@ -1,33 +1,56 @@
-var queries = [
-    {
-        context: 'mobile',
-        match: function() {
-            $("body").addClass("mobile");
-            // Your mobile specific logic can go here. 
+
+(function(MQ, $, window, document) {
+
+    "use strict";
+
+    var queries = [
+        {
+            context: 'mobile',
+            match: function() {
+                $("body").addClass("mobile");
+                // Your mobile specific logic can go here. 
+            },
+            unmatch: function() {
+                $("body").removeClass("mobile");
+        
+            }
         },
-        unmatch: function() {
-            $("body").removeClass("mobile");
-    
-        }
-    },
-    {
-        context: 'skinny',
-        match: function() {
-            console.log('skinny callback! Swap the class on the body element.');
-            // Your tablet specific logic can go here.
+        {
+            context: 'skinny',
+            match: function() {
+                console.log('skinny callback! Swap the class on the body element.');
+                // Your tablet specific logic can go here.
+            },
+            unmatch: function() {
+                console.log('leaving skinny context!');
+            }
+
         },
-        unmatch: function() {
-            console.log('leaving skinny context!');
+        {
+            context: 'wide-screen',
+            match: function() {
+                console.log('wide-screen callback woohoo! Load some heavy desktop JS badddness.');
+                // your desktop specific logic can go here.
+            }
         }
 
-    },
-    {
-        context: 'wide-screen',
-        match: function() {
-            console.log('wide-screen callback woohoo! Load some heavy desktop JS badddness.');
-            // your desktop specific logic can go here.
+    ];
+
+    // Go!
+    MQ.init(queries);
+
+    $(window).scroll(function() {
+
+        var scroll = $(window).scrollTop();
+
+        if (scroll > 500) {
+            if (!$('.top').hasClass('scrolled'))
+                $('.top').addClass('scrolled')
+        } else {
+            if ($('.top').hasClass('scrolled'))
+                $('.top').removeClass('scrolled')
         }
-    }
-];
-// Go!
-MQ.init(queries);
+
+    });
+
+})(MQ, $, window, document);
